66a62eb7288db92705a35b4c1950ab52eaeff03e
mutazzahdeh <mutazzahdeh@gmail.com>
2021-08-09 23:30:10 +0300

    Header

---
+++ Project/client/src/component/Heder.js
@@ -0,0 +1,16 @@
+import { CardHeader,Paper,Card } from '@material-ui/core';
+
+
+const Heder = () => {
+    return (
+        <div>
+            
+            <Card>
+        <CardHeader title={"zxxfdsfdsf"} />
+        
+      </Card>
+        </div>
+    )
+}
+
+export default Heder
