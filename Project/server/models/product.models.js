aa9e389a1f59305b6914ebb11eae22a754d89ae4
lana yaghi <yaghilana.alaa@gmail.com>
2021-08-09 19:18:03 +0300

    models

--- Project/server/models/product.models.js
+++ Project/server/models/product.models.js
@@ -13,14 +13,36 @@ const ProductSchema = new mongoose.Schema({
     desc: {
         type: String
     } ,
-    status:{
+    status:{ 
+        type : String,
+        default : "inStock"
         
     },
     img: 
         { data: Buffer, contentType: String },
+        
+    category : {
+        type : String,
+        enum : ["itemA","itemE", "itemC", "itemD","itemM", "itemB", "itemS"],
+        default: "itemA"
+    }
     
 
 },{ timestamps: true }) 
 
+const NewsSchema = new mongoose.Schema({
+	title:  {
+        type: String,
+    },
+	desc: {
+        type: String
+    } ,
+    date: {
+        type: Date
+    } 
+},{ timestamps: true }) 
+
 
 module.exports.Product = mongoose.model("Product", ProductSchema);
+
+module.exports.News = mongoose.model("News", NewsSchema);
