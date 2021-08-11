1b4fbddc2dabe9ca56ed3ec95ccd6eb1f081ba5c
lana yaghi <yaghilana.alaa@gmail.com>
2021-08-11 16:30:54 +0300

    header comp

--- Project/client/src/App.js
+++ Project/client/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import Heder from './components/Heder';
+import Heder from './components/Footer';
+import Footer from './components/Navbar';
 import NewsForm from './components/NewsForm';
 import {Router} from '@reach/router'
 import Main from './views/Main'
@@ -8,21 +9,23 @@ import NewsScroll from './components/NewsScroll';
 import NewsMain from './views/NewsMain';
 import {Aboutus} from "./views/Aboutus"
 
+import Navbar from './components/Navbar';
+
+import useStyles from './components/styles';
 
 
 function App() {
+  const classes = useStyles();
   return (
-    <>    
-    <Router>
-    
-    <NewsMain path="/news" />
-     <Main path="/show"></Main>
-     <Aboutus path='/aboutus'></Aboutus>
-     
-   </Router>
-    </>
-
-
+    <div className="App">  
+      <Navbar/>
+      <Router>
+        <NewsMain path="/news" />
+        <Main path="/show"></Main>
+        <Aboutus path='/aboutus'></Aboutus>
+      </Router>
+      <Heder/>
+  </div>
   );
 }
 
