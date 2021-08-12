cc5496a0591f5bacdc525faf745df28485a123c1
mutazzahdeh <mutazzahdeh@gmail.com>
2021-08-12 11:00:45 +0300

    Editing

--- Project/client/src/App.js
+++ Project/client/src/App.js
@@ -1,33 +1,31 @@
-
 import './App.css';
-import Heder from './components/Footer';
-import Footer from './components/Navbar';
-import NewsForm from './components/NewsForm';
-import {Router} from '@reach/router'
+import { Router } from '@reach/router'
 import Main from './views/Main'
-import Show from './views/Show'
-import NewsScroll from './components/NewsScroll';
 import NewsMain from './views/NewsMain';
-import {Aboutus} from "./views/Aboutus"
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import useStyles from './components/styles';
+import Show from './views/Show';
+import NewsScroll from './components/NewsScroll';
+import { Aboutus } from "./views/Aboutus"
 
 
 
 function App() {
   const classes = useStyles();
   return (
-    <>    
-    
-    <div className="App">  
-      <Navbar/>
-      <Router>
-        <NewsMain path="/news" />
-        <Main path="/show"></Main>
-        <Aboutus path='/aboutus'></Aboutus>
-        <Show path='/kalb'/>
-      </Router>
-      <Heder/>
-  </div>
-   </>
+    <>
+
+      <div className="App">
+        <Navbar />
+        <Router>
+          <NewsMain path="/news" />
+          <Main path="/show"></Main>
+          <Aboutus path='/aboutus'></Aboutus>
+          <Show path='/kalb' />
+        </Router>
+        <Footer />   </div>
+    </>
 
   );
 }
