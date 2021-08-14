11bda2e73a6df34e785be383e97e3162447b7232
Ghazalsal <ghazalsalameh@hotmail.com>
2021-08-14 20:27:47 +0300

    changes

--- Project/client/src/views/AdminSide.js
+++ Project/client/src/views/AdminSide.js
@@ -2,13 +2,13 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 export const AdminSide = (props) => {
     const {children}=props
-    const link={"/product":" اضافة او حذف منج","/news/new":"اضافة او حذف خبر","talbat/":" اتصل بنا"}
+    const link={"/product":" اضافة او حذف منتج","/news/new":"اضافة او حذف خبر","talbat/":" اتصل بنا"}
 
     return (
         <div>
             <Navbar LinkTo={link}/>
             {children}
-            <p>Counter</p>
+            
             <Footer/>
             
         </div>
