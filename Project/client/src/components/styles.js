7c33a84a9fde395f316fe459ab224e1058569d5c
lana yaghi <yaghilana.alaa@gmail.com>
2021-08-14 15:29:04 +0300

    About Us finish

--- Project/client/src/components/styles.js
+++ Project/client/src/components/styles.js
@@ -1,12 +1,14 @@
 import { colors } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 import { makeStyles, withTheme } from '@material-ui/core/styles';
+import img from '../images/abou.jpg';
+
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-        backgroundImage: 'linear-gradient(45deg, #901414 30%, white 80%)',
+        // backgroundImage: 'linear-gradient(45deg, #901414 30%, white 80%)',
         
     },
     customizeToolbar: {
@@ -34,7 +36,8 @@ const useStyles = makeStyles((theme) => ({
 
     img : {
         width: "100px",
-        height : "100px",
+        height : "100px"
+        
 
     },
     header: {
@@ -68,8 +71,83 @@ const useStyles = makeStyles((theme) => ({
         fontSize: "12px",
         backgroundColor: "#6d0909",
         padding: "10px",
-        color: "white"
+        color: "white",
+    },
+
+//aboutUs Page
+    container :{
+        // backgroundImage:  `url(${})`,
+        width: "100%",
+        // height: "1600px",
+        backdropFilter: "contrast(0.95)"
+    },
+    
+    About: {
+    margin : "20px",
+    textAlign: "center",
+    color: "black"
+    }, 
+
+    body: {
+    display :"flex",
+    textAlign: "center",
+    justifyContent: "center",
+    margin: "10px",
+    color : " balck"
+    },
+    
+    cardOne : {
+    margin: "10px",
+    width: "300px",
+    height: "300px",
+    color: "balck",
+    background: "white",
+    textAlign: "center"
+    
+    },
+    
+    carThree : {
+        margin: "10px",
+        width: "300px",
+        height: "300px",
+        background: "white",
+        textAlign: " center"
         
-    }
+    },
+
+    cardFour : {
+        margin: "10px",
+        width: "300px",
+        height: "300px",
+        background: "white",
+        textAlign: "center"
+        
+    },
+    cardTwo: {
+        height:"auto",
+        backgroundColor: "white",
+        width: "70%",
+        padding:"30px",
+        fontSize:"15px",
+        marginRight:"70px",
+        marginBottom: "20px",
+        marginTop: "10px",
+        textAlign: "right",
+        listStylePosition: "inside",
+        marginRight: "150px"
+        
+
+    },
+
+
+
+    icon :{
+        width: "50px",
+        height: "50px",
+        textAlign: "center",
+        // marginR:"0px auto"
+        marginTop:"10px"
+        }
+
 }));
 export default useStyles;
